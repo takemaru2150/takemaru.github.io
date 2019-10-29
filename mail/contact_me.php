15b5a36ffff6a400c405a4687467d73352b814fb
sakurai <takeshi2jp1000@gmail.com>
2019-10-29 20:19:28 +0900

    contact.phpのif文の修正

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,11 +18,20 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'takemaru2150@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Takeshi Sakurai のお問い合わせフォーム:  $name";
-$email_body = "あなたのウェブサイトの連絡フォームから新しいメッセージを受け取りました。\n\n"."＝＝＝＝＝＝＝＝＝>＝＝＝:\n\nお名前: $name\n\nメールアドレス: $email_address\n\n電話番号: $phone\n\nメッセージ:\n$message";
-$headers = "From: kjj418sakurai@icloud.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+if(isset($_POST['submit'])){
+
+   $to = 'takemaru2150@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+   $email_subject = "Takeshi Sakurai のお問い合わせフォーム:  $name";
+   $email_body = "あなたのウェブサイトの連絡フォームから新しいメッセージを受け取りました。\n\n"."＝＝＝＝＝＝＝＝＝>＝＝＝:\n\nお名前: $name\n\nメールアドレス: $email_address\n\n電話番号: $phone\n\nメッセージ:\n$message";
+   $headers = "From: kjj418sakurai@icloud.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+   $headers .= "Reply-To: $email_address";   
+   $go = mail($to,$email_subject,$email_body,$headers);
+
+   if(!$go) {   
+      echo "Something went wrong.";// error
+   } else {
+      echo "Message was send";// correct
+   }
+}
+// return true;
 ?>
