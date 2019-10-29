3a1f19b94fb1ddad047908b5727f16317c22cd25
sakurai <takeshi2jp1000@gmail.com>
2019-10-29 17:55:02 +0900

    mb_languageの追加

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,6 @@
 <?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -19,7 +21,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'takemaru2150@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Takeshi Sakurai のお問い合わせフォーム:  $name";
 $email_body = "あなたのウェブサイトの連絡フォームから新しいメッセージを受け取りました。\n\n"."＝＝＝＝＝＝＝＝＝>＝＝＝:\n\nお名前: $name\n\nメールアドレス: $email_address\n\n電話番号: $phone\n\nメッセージ:\n$message";
-$headers = "From: takemaru2150@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: kjj418sakurai@icloud.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
