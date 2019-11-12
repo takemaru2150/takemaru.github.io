afd7ce0441108d4dd9faef7e49c22099f4197b4b
sakurai <takeshi2jp1000@gmail.com>
2019-11-12 13:10:54 +0900

    不要部分の削除

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
-// servicesとportfolioセクションのjsスクロールアニメーション
+// servicesとaboutセクションのjsスクロールアニメーション
 $(function(){
-
   $(window).scroll(function (){
     var fadeTrigger = $('.js-trigger');
     $(fadeTrigger).each(function(){
@@ -9,17 +8,13 @@ $(function(){
           windowHeight = $(window).height();
 
       if (scroll > elemTop - windowHeight / 1.1){
-        $(this).addClass('fade-scale');
-        if ($(this).hasClass('multi-trigger')) {
-          $(this).find('.fade-elem').addClass('fade-up-down');
-        } else if ($(this).hasClass('fade-type-up') || $(this).hasClass('fade-type-down')) {
-          $(this).addClass('fade-up-down');
-        } else if ($(this).hasClass('fade-type-left') || $(this).hasClass('fade-type-right')) {
+        if ($(this).hasClass('fade-type-left') || $(this).hasClass('fade-type-right')) {
           $(this).addClass('fade-left-right');
-        }
+          continue;
+        };
+        $(this).addClass('fade-scale');
       }
     });
   });
   $(window).trigger('scroll');
-
 });
